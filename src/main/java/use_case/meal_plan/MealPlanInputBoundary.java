aae833c350f974dafe1722e9fafcb569720c0228
Matthew <matthew.giacomini@mail.utoronto.ca>
2025-11-15 17:06:49 -0500

    Implemented the Input Boundary and Input Data for the Meal Plan Use Case.

--- src/main/java/use_case/meal_plan/MealPlanInputBoundary.java
+++ src/main/java/use_case/meal_plan/MealPlanInputBoundary.java
@@ -1,4 +1,13 @@
 package use_case.meal_plan;
 
+/**
+ * Input Boundary for the Meal Plan Use Case
+ */
 public interface MealPlanInputBoundary {
+
+    /**
+     * Executes the meal plan use case
+     * @param mealPlanInputData the input data
+     */
+    void execute(MealPlanInputData mealPlanInputData);
 }
