40bc492ca1e6eb2f6eb3673f9616602e1440f371
Matthew <matthew.giacomini@mail.utoronto.ca>
2025-11-15 17:06:49 -0500

    Implemented the Input Boundary and Input Data for the Meal Plan Use Case.

--- src/main/java/use_case/meal_plan/MealPlanInputData.java
+++ src/main/java/use_case/meal_plan/MealPlanInputData.java
@@ -1,4 +1,27 @@
 package use_case.meal_plan;
 
+/**
+ * The Input Data for the Meal Plan Use Case
+ */
 public class MealPlanInputData {
+
+    private final int targetCalories;
+    private final int targetProtein;
+    private final int targetCarbs;
+    private final int targetFats;
+
+    public MealPlanInputData(int targetCalories, int targetProtein, int targetCarbs, int targetFats) {
+        this.targetCalories = targetCalories;
+        this.targetProtein = targetProtein;
+        this.targetCarbs = targetCarbs;
+        this.targetFats = targetFats;
+    }
+
+    int getTargetCalories() {return  targetCalories;}
+
+    int getTargetProtein() {return  targetProtein;}
+
+    int getTargetCarbs() {return  targetCarbs;}
+
+    int getTargetFats() {return  targetFats;}
 }
