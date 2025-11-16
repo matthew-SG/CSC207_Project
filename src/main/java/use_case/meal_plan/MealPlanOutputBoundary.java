aab711dafaef60389501913241dd3795fcb2942c
Matthew <matthew.giacomini@mail.utoronto.ca>
2025-11-15 19:24:46 -0500

    Implemented Output Boundary and Output Data for Meal Plan Use Case

--- src/main/java/use_case/meal_plan/MealPlanOutputBoundary.java
+++ src/main/java/use_case/meal_plan/MealPlanOutputBoundary.java
@@ -1,4 +1,19 @@
 package use_case.meal_plan;
 
+/**
+ * The Output Boundary for the Meal Plan Use Case.
+ */
 public interface MealPlanOutputBoundary {
+
+    /**
+     * Prepares the success view for the Meal Plan Use Case.
+     * @param mealPlanOutputData the output data
+     */
+    void prepareSuccessView(MealPlanOutputData mealPlanOutputData);
+
+    /**
+     * Prepares the failure view for the Meal Plan Use Case.
+     * @param errorMessage the explanation of the failure
+     */
+    void prepareFailView(String errorMessage);
 }
