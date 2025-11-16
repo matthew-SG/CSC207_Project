5d5c4d62e9d56300d5c85232e79da9ef9d0c5f56
Matthew <matthew.giacomini@mail.utoronto.ca>
2025-11-15 23:23:12 -0500

    Implemented getters for Recipe entity

--- src/main/java/entities/Recipe.java
+++ src/main/java/entities/Recipe.java
@@ -1,12 +1,24 @@
 package entities;
-import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class Recipe {
     private int recipeId;
     private String recipeName;
     private String recipeImage;
-    private ArrayList<Ingredient> ingredients;
+    private List<Ingredient> ingredients;
     private Map<String, Double> nutritionalValues;
     private String mealType;
+
+    public int getRecipeId() { return recipeId; }
+
+    public String getRecipeName() { return recipeName; }
+
+    public String getRecipeImage() { return recipeImage; }
+
+    public List<Ingredient> getIngredients() { return ingredients; }
+
+    public Map<String, Double> getNutritionalValues() { return nutritionalValues; }
+
+    public String getMealType() { return mealType; }
 }
