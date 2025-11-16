309af9feb05780c28a17079ebeb338311389616b
Matthew <matthew.giacomini@mail.utoronto.ca>
2025-11-16 14:34:10 -0500

    Implemented Meal Plan Controller

--- src/main/java/interface_adapter/meal_plan/MealPlanController.java
+++ src/main/java/interface_adapter/meal_plan/MealPlanController.java
@@ -1,4 +1,30 @@
 package interface_adapter.meal_plan;
 
+import use_case.meal_plan.MealPlanInputBoundary;
+import use_case.meal_plan.MealPlanInputData;
+
+/**
+ * Controller for the Meal Plan Use Case
+ */
 public class MealPlanController {
+    private final MealPlanInputBoundary userMealPlanUseCaseInteractor;
+
+    public MealPlanController(MealPlanInputBoundary userMealPlanUseCaseInteractor) {
+        this.userMealPlanUseCaseInteractor = userMealPlanUseCaseInteractor;
+    }
+
+    /**
+     * Executes the Meal Plan Use Case
+     * @param targetCalories the target calories
+     * @param targetProtein the target protein
+     * @param targetCarbs the target carbs
+     * @param targetFats the target fats
+     */
+    public void execute(double targetCalories, double targetProtein, double targetCarbs, double targetFats) {
+        final MealPlanInputData mealPlanInputData = new MealPlanInputData(targetCalories, targetProtein, targetCarbs,
+                targetFats);
+
+        userMealPlanUseCaseInteractor.execute(mealPlanInputData);
+    }
+
 }
