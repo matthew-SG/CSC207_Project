4d26b0a446e7241dc5a572e928376393fa5fc6f4
Matthew <matthew.giacomini@mail.utoronto.ca>
2025-11-16 16:03:32 -0500

    Implemented View Models for Meal Plan Use Case (?)

---
+++ src/main/java/interface_adapter/meal_plan/MealPlanGeneratorViewModel.java
@@ -0,0 +1,22 @@
+package interface_adapter.meal_plan;
+
+import interface_adapter.ViewModel;
+
+/**
+ * The View Model for the Meal Plan generator
+ */
+public class MealPlanGeneratorViewModel extends ViewModel<MealPlanState> {
+
+    private static final String TITLE_LABEL = "Meal Plan Generator View";
+    private static final String CALORIES_LABEL = "Enter target calories for the day";
+    private static final String PROTEIN_LABEL = "Enter target protein for the day (in grams)";
+    private static final String CARBS_LABEL = "Enter target carbs for the day (in grams)";
+    private static final String FATS_LABEL =  "Enter target fats for the day (in grams)";
+
+    private static final String GENERATE_LABEL = "Generate";
+
+    public MealPlanGeneratorViewModel() {
+        super("meal plan generator");
+        setState(new MealPlanState());
+    }
+}
