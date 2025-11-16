110cc83d33c5010babebba43a2e15600e2ad3688
Matthew <matthew.giacomini@mail.utoronto.ca>
2025-11-15 21:38:00 -0500

    Made changes to Output Data, began implementing Interactor, and implemented DAI for Meal Plan Use Case

--- src/main/java/use_case/meal_plan/MealPlanInteractor.java
+++ src/main/java/use_case/meal_plan/MealPlanInteractor.java
@@ -1,4 +1,31 @@
 package use_case.meal_plan;
 
-public class MealPlanInteractor {
+import entities.Recipe;
+
+import java.util.List;
+
+/**
+ * The Meal Plan Use Case Interactor
+ */
+public class MealPlanInteractor implements MealPlanInputBoundary{
+    private final MealPlanUserDataAccessInterface userDataAccessObject;
+    private final MealPlanOutputBoundary mealPlanPresenter;
+
+    public MealPlanInteractor(MealPlanUserDataAccessInterface userDataAccessObject,
+                              MealPlanOutputBoundary mealPlanPresenter) {
+        this.userDataAccessObject = userDataAccessObject;
+        this.mealPlanPresenter = mealPlanPresenter;
+    }
+
+    public void execute(MealPlanInputData mealPlanInputData) {
+        final List<Recipe> savedRecipes = userDataAccessObject.getSavedRecipes();
+        if (savedRecipes.size() < 3) {
+            mealPlanPresenter.prepareFailView("At least 3 saved recipes must be saved for meal plan generation.");
+        } else {
+            final int calories = mealPlanInputData.getTargetCalories();
+            final int protein = mealPlanInputData.getTargetProtein();
+            final int carbs = mealPlanInputData.getTargetCarbs();
+            final int fats = mealPlanInputData.getTargetFats();
+        }
+    }
 }
