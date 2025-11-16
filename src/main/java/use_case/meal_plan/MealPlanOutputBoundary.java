98da0bc457e8d6f162689b9ef8cd1ccafd48a381
Matthew <matthew.giacomini@mail.utoronto.ca>
2025-11-16 18:39:54 -0500

    Updated fail view for Meal Plan Use Case

--- src/main/java/use_case/meal_plan/MealPlanOutputBoundary.java
+++ src/main/java/use_case/meal_plan/MealPlanOutputBoundary.java
@@ -12,8 +12,17 @@ public interface MealPlanOutputBoundary {
     void prepareSuccessView(MealPlanOutputData mealPlanOutputData);
 
     /**
-     * Prepares the failure view for the Meal Plan Use Case.
-     * @param errorMessage the explanation of the failure
+     * Prepares the fail view for the Meal Plan Use Case
+     * @param error the error to be displayed
      */
-    void prepareFailView(String errorMessage);
+    void prepareFailView(String error);
+    
+    /**
+     * Prepares the fail view for the Meal Plan Use Case
+     * @param caloriesError the explanation of the error for the calories input
+     * @param proteinError the explanation of the error for the protein input
+     * @param carbsError the explanation of the error for the carbs input
+     * @param fatsError the explanation of the error for the fats input
+     */
+    void prepareFailView(String caloriesError, String proteinError, String carbsError, String fatsError);
 }
