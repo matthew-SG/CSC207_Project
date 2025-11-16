a894577ac681f995ae066a00b3c339e07222d5ed
Matthew <matthew.giacomini@mail.utoronto.ca>
2025-11-16 00:21:29 -0500

    Began implementing helper method that computes the best three recipes for meal plan. As part of that, implemented helper method that computes all unique recipe triplets from a list of recipes.

--- src/main/java/use_case/meal_plan/MealPlanInteractor.java
+++ src/main/java/use_case/meal_plan/MealPlanInteractor.java
@@ -4,7 +4,9 @@ import entities.Ingredient;
 import entities.MealPlan;
 import entities.Recipe;
 
+import java.sql.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -29,7 +31,7 @@ public class MealPlanInteractor implements MealPlanInputBoundary{
         final int fats = mealPlanInputData.getTargetFats();
         String[] recipeNames = new String[3];
         String[] recipeImages = new String[3];
-        List<Ingredient> recipeIngredients = new ArrayList<>();
+        List<List<Ingredient>> recipeIngredients = new ArrayList<>();
         List<Map<String, Double>> recipeNutritionalValues = new ArrayList<>();
         if (savedRecipes.size() < 3) {
             mealPlanPresenter.prepareFailView("At least 3 saved recipes must be saved for meal plan generation.");
@@ -40,10 +42,62 @@ public class MealPlanInteractor implements MealPlanInputBoundary{
             userDataAccessObject.saveMealPlan(mealPlan);
             int i = 0;
             for (Recipe recipe : savedRecipes) {
-
+                recipeNames[i] = recipe.getRecipeName();
+                recipeImages[i] = recipe.getRecipeImage();
+                recipeIngredients.add(recipe.getIngredients());
+                recipeNutritionalValues.add(recipe.getNutritionalValues());
+                i++;
             }
 
+            MealPlanOutputData mealPlanOutputData = new MealPlanOutputData(recipeNames, recipeImages, recipeIngredients,
+                    recipeNutritionalValues);
+            mealPlanPresenter.prepareSuccessView(mealPlanOutputData);
+        } else {
+
         }
+    }
+
+    /**
+     * Helper function that computes the best fitting recipes for the designated meal plan
+     * @param recipes the user's currently saved recipes (with recipes.size() > 3)
+     * @return the three recipes that fit closest to the meal plan parameters
+     */
+    private List<Recipe> computeBestFittingRecipes(List<Recipe> recipes) {
+        assert recipes.size() > 3;
+
+        List<List<Recipe>> recipeTriplets = createTripletCombinations(recipes);
+
+    }
+
+    /**
+     * Helper function that creates the n choose 3 combinations of recipe triplets out of the overall list of recipes
+     * recipe, where n = recipe.size() > 3.
+     * @param recipes the overall/total list of individual recipes
+     * @return the unique triplet combinations of recipes
+     */
+    private static List<List<Recipe>> createTripletCombinations(List<Recipe> recipes) {
+        assert recipes.size() > 3;
+        ArrayList<List<Recipe>> recipeTriplets = new ArrayList<>();
+        ArrayList<Recipe> recipeTriplet = new ArrayList<>();
+        int n = recipes.size();
+
+        for (int i = 0; i < n - 2; i++) {
+            recipeTriplet.add(recipes.get(i));
+            for (int j = i + 1; j < n - 1; j++) {
+                recipeTriplet.add(recipes.get(j));
+                for (int k = j + 1; k < n; k++) {
+                    recipeTriplet.add(recipes.get(k));
+                    recipeTriplets.add(recipeTriplet);
+                    recipeTriplet.remove(2);
+                    
+                }
+                recipeTriplet.remove(1);
+
+            }
+            recipeTriplet.remove(0);
+        }
+
+        return recipeTriplets;
     }
 
 }
