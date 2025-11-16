99e3109b7d3149c3b74219e61d9a5dd30237e7e3
Matthew <matthew.giacomini@mail.utoronto.ca>
2025-11-16 17:19:30 -0500

    Implemented Presenter (and some other changes) (?)

--- src/main/java/interface_adapter/meal_plan/MealPlanGeneratorState.java
+++ src/main/java/interface_adapter/meal_plan/MealPlanGeneratorState.java
@@ -5,9 +5,13 @@ package interface_adapter.meal_plan;
  */
 public class MealPlanGeneratorState {
     private String targetCalories = "";
+    private String caloriesError;
     private String targetProtein = "";
+    private String proteinError;
     private String targetCarbs = "";
+    private String carbsError;
     private String targetFats = "";
+    private String fatsError;
 
     public String getTargetCalories() {
         return targetCalories;
@@ -15,15 +19,31 @@ public class MealPlanGeneratorState {
 
     public void setTargetCalories(String targetCalories) { this.targetCalories = targetCalories; }
 
+    public String getCaloriesError() { return caloriesError; }
+
+    public void setCaloriesError(String caloriesError) { this.caloriesError = caloriesError; }
+
     public String getTargetProtein() { return targetProtein; }
 
     public void setTargetProtein(String targetProtein) { this.targetProtein = targetProtein; }
+
+    public String getProteinError() { return proteinError; }
+
+    public void setProteinError(String proteinError) { this.proteinError = proteinError; }
 
     public String getTargetCarbs() { return targetCarbs; }
 
     public void setTargetCarbs(String targetCarbs) { this.targetCarbs = targetCarbs; }
 
+    public String getCarbsError() { return carbsError; }
+
+    public void setCarbsError(String carbsError) { this.carbsError = carbsError; }
+
     public String getTargetFats() { return targetFats; }
 
     public void setTargetFats(String targetFats) { this.targetFats = targetFats; }
+
+    public String getFatsError() { return fatsError; }
+
+    public void setFatsError(String fatsError) {  this.fatsError = fatsError; }
 }
