7ccfaeb0f5877e2f2fb225dd097f11cc193599d3
Matthew <matthew.giacomini@mail.utoronto.ca>
2025-11-16 23:41:43 -0500

    deleted some stuff in the generator view model

--- src/main/java/interface_adapter/meal_plan/MealPlanGeneratorViewModel.java
+++ src/main/java/interface_adapter/meal_plan/MealPlanGeneratorViewModel.java
@@ -7,14 +7,6 @@ import interface_adapter.ViewModel;
  */
 public class MealPlanGeneratorViewModel extends ViewModel<MealPlanGeneratorState> {
 
-    private static final String TITLE_LABEL = "Meal Plan Generator View";
-    private static final String CALORIES_LABEL = "Enter target calories for the day";
-    private static final String PROTEIN_LABEL = "Enter target protein for the day (in grams)";
-    private static final String CARBS_LABEL = "Enter target carbs for the day (in grams)";
-    private static final String FATS_LABEL =  "Enter target fats for the day (in grams)";
-
-    private static final String GENERATE_LABEL = "Generate";
-
     public MealPlanGeneratorViewModel() {
         super("meal plan generator");
         setState(new MealPlanGeneratorState());
