893da79f29a84029072d3686164e4db85a1e911f
Adish <adish.singh@mail.utoronto.ca>
2025-11-23 16:20:19 -0500

    Updated the UI layout to have the step logo and instructions vertical to each other, and also included a picture of a chef along with it for the step-by-step view

--- src/main/java/view/StepByStepView.java
+++ src/main/java/view/StepByStepView.java
@@ -19,6 +19,11 @@ public class StepByStepView extends JFrame implements PropertyChangeListener {
     private final StepByStepViewModel stepByStepViewModel;
     private final SpeechService speechService;
 
+    private ImageIcon scaledIcon;
+    private Image scaled;
+    private Image resized;
+    private JLabel imageLabel;
+    private ImageIcon chefIcon;
     private JLabel stepLabel;
     private JTextArea stepTextArea;
     private JButton prevButton;
@@ -44,15 +49,39 @@ public class StepByStepView extends JFrame implements PropertyChangeListener {
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setLayout(new BorderLayout());
 
-        stepLabel = new JLabel("Step 1", SwingConstants.CENTER);
-        stepLabel.setFont(new Font("Arial", Font.BOLD, 18));
-        add(stepLabel, BorderLayout.NORTH);
+        stepLabel = new JLabel("Step 1");
 
         stepTextArea = new JTextArea();
         stepTextArea.setEditable(false);
         stepTextArea.setLineWrap(true);
         stepTextArea.setWrapStyleWord(true);
-        add(new JScrollPane(stepTextArea), BorderLayout.CENTER);
+
+        chefIcon = new ImageIcon("src/main/java/view/chef-cooking-kitchen-while-wearing-professional-attire.jpg");
+        resized = chefIcon.getImage();
+        int newWidth = 200;
+        int newHeight = 140;
+        scaled = resized.getScaledInstance(newWidth, newHeight, Image.SCALE_SMOOTH);
+        scaledIcon = new ImageIcon(scaled);
+        imageLabel = new JLabel(scaledIcon);
+
+        JPanel stepButtonPanel = new JPanel(new GridBagLayout());
+
+        GridBagConstraints gbc = new GridBagConstraints();
+
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        gbc.weightx = 0.1;
+        stepButtonPanel.add(imageLabel, gbc);
+
+        gbc.gridx = 2;
+        gbc.gridy = 0;
+        stepButtonPanel.add(stepLabel, gbc);
+
+        gbc.gridx = 2;
+        gbc.gridy = 1;
+        stepButtonPanel.add(stepTextArea, gbc);
+
+        add(stepButtonPanel, BorderLayout.CENTER);
 
         prevButton = new JButton("Previous Step");
         nextButton = new JButton("Next Step");
