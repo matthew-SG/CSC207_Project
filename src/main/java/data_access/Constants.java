7c4e6d64c360216df8f30f52207615230d077b2c
Jingtian Hu <jt.hu@mail.utoronto.ca>
2025-11-24 15:19:57 -0500

    added missing Constants class

---
+++ src/main/java/data_access/Constants.java
@@ -0,0 +1,6 @@
+package data_access;
+
+public class Constants {
+    public static String WEBAPI_KEY = "AIzaSyDRtQtANy7zkSqpmO9CXQDiLFl7cU5uLeU";
+    public static String AUTH_ENDPOINT = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDRtQtANy7zkSqpmO9CXQDiLFl7cU5uLeU&";
+}
