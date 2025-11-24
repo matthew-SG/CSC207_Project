ee819f2a4b2063120dd8380d01651eea70752d83
klyvux <ab.bajandoh@gmail.com>
2025-11-24 16:39:02 -0500

    made the api only gets the data moved all filtering stuff to interaction

--- src/main/java/use_case/search_by_ingr/SearchByIngredientInteractor.java
+++ src/main/java/use_case/search_by_ingr/SearchByIngredientInteractor.java
@@ -3,6 +3,9 @@ package use_case.search_by_ingr;
 import API.SearchByIngredientSpoonacular;
 import entities.Ingredient;
 import entities.Recipe;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.*;
 
 import java.util.*;
 
@@ -16,10 +19,83 @@ public class SearchByIngredientInteractor implements SearchByIngredientInputBoun
     public SearchByIngredientOutputData execute(SearchByIngredientInputData inputData) {
         List<Ingredient> ingredients = inputData.getIngredients();
         if(ingredients.isEmpty()){return new SearchByIngredientOutputData(List.of(),"Enter at least one ingredient");}
-        List<Recipe> recipes = api.searchByIngredientSpoonacular(ingredients);
+        JSONArray results= api.searchByIngredientSpoonacular(ingredients);
+        ArrayList<Recipe> recipes=new ArrayList<>();
+
+        System.out.println(results.toString());
+
+        for (int i = 0; i < results.length() && recipes.size() < 5; i++) {
+            JSONObject object = results.getJSONObject(i);
+            int missedIngredients = object.getInt("missedIngredientCount");
+            if (missedIngredients == 0) {
+                JSONArray usedIngredients = object.getJSONArray("usedIngredients");
+                boolean haveEnough = true;
+                for (int j = 0; j < usedIngredients.length()&&haveEnough; j++) {
+                    JSONObject usedIngredient = usedIngredients.getJSONObject(j);
+                    double amount = usedIngredient.getDouble("amount");
+                    String name = usedIngredient.getString("name").toLowerCase();
+                    String[] unitName = new String[3];
+                    unitName[0] = usedIngredient.getString("unit");
+                    unitName[1] = usedIngredient.getString("unitShort");
+                    unitName[2] = usedIngredient.getString("unitLong");
+                    for (Ingredient ingredient : ingredients) {
+                        if (ingredient.getName().toLowerCase().contains(name)) {
+                            if (ingredient.getQuantity() < amount &&
+                                            (ingredient.getUnit().equals(unitName[0])
+                                            ||ingredient.getUnit().equals(unitName[1])
+                                            ||ingredient.getUnit().equals(unitName[2])) ) {
+                                haveEnough = false;
+                                break;
+                            }
+                        }
+                    }
+                }
+                if (haveEnough) {
+                    Recipe recipe = new Recipe(
+                            object.getInt("id"),
+                            object.getString("title"),
+                            object.getString("image"),
+                            "N/A");
+                    recipes.add(recipe);
+                }
+            }
+        }
+
+
         String msg;
         if(recipes.isEmpty()) msg="no perfect match found";
         else msg="found";
         return new SearchByIngredientOutputData(recipes,msg);
     }
 }
+
+/*for (int i = 0; i < results.length() && result.size() < 5; i++) {
+JSONObject object = results.getJSONObject(i);
+int missedIngredients = object.getInt("missedIngredientCount");
+                if (missedIngredients == 0) {
+JSONArray usedIngredients = object.getJSONArray("usedIngredients");
+boolean haveEnough = true;
+                    for (int j = 0; j < usedIngredients.length()&&haveEnough; j++) {
+JSONObject usedIngredient = usedIngredients.getJSONObject(j);
+double amount = usedIngredient.getDouble("amount");
+String name = usedIngredient.getString("name").toLowerCase();
+                        for (Ingredient ingredient : ingredients) {
+        if (ingredient.getName().toLowerCase().contains(name)) {
+        if (ingredient.getQuantity() < amount) {
+haveEnough = false;
+        break;
+        }
+        }
+        }
+        }
+        if (haveEnough) {
+Recipe recipe = new Recipe(
+        object.getInt("id"),
+        object.getString("title"),
+        object.getString("image"),
+        "N/A");
+                        result.add(recipe);
+                    }
+                            }
+                            }
+                            return result;*/
