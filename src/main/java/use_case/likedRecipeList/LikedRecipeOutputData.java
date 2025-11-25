3fa2c9460c88ecdd8c285b5922b9f348ed552e4e
Adish <adish.singh@mail.utoronto.ca>
2025-11-24 20:52:10 -0500

    Using the feedback, I instead defined my own input data, not dependent on the recipe entity, so that we can follow the SOLID principles.

--- src/main/java/use_case/likedRecipeList/LikedRecipeOutputData.java
+++ src/main/java/use_case/likedRecipeList/LikedRecipeOutputData.java
@@ -2,10 +2,29 @@ package use_case.likedRecipeList;
 
 import entities.Recipe;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
-public record LikedRecipeOutputData(ArrayList<Recipe> recipes, String message, boolean success) {
+public class LikedRecipeOutputData {
+    private final String[] recipeNames;
+    private final List<List<String[]>> recipeIngredients;
+    private final List<List<String[]>> recipeSteps;
 
-    public ArrayList<Recipe> getUpdatedList() {return recipes;}
-    public String getMessage() { return message; }
-    public boolean isSuccess() { return success; }
+    public LikedRecipeOutputData(String[] recipeNames, List<List<String[]>> ingredients, List<List<String[]>> steps) {
+        this.recipeNames = recipeNames;
+        this.recipeIngredients = ingredients;
+        this.recipeSteps = steps;
+    }
+
+    public String[] getRecipeNames() {
+        return recipeNames;
+    }
+
+    public List<List<String[]>> getRecipeIngredients() {
+        return recipeIngredients;
+    }
+
+    public List<List<String[]>> getRecipeSteps() {
+        return recipeSteps;
+    }
 }
