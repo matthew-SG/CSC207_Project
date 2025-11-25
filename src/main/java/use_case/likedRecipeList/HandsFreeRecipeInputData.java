bb21cf734deae6586bf576402a346aae60ad0942
Matthew <matthew.giacomini@mail.utoronto.ca>
2025-11-25 12:20:05 -0500

    Renamed HandsFreeRecipeInputData object to properly match object naming convention.

--- src/main/java/use_case/likedRecipeList/HandsFreeRecipeInputData.java
+++ src/main/java/use_case/likedRecipeList/HandsFreeRecipeInputData.java
@@ -1,9 +1,9 @@
 package use_case.likedRecipeList;
 
-public class HandsfreeRecipeInputData {
+public class HandsFreeRecipeInputData {
     private final String RecipeId;
 
-    public HandsfreeRecipeInputData(String recipeId) {
+    public HandsFreeRecipeInputData(String recipeId) {
         this.RecipeId = recipeId;
     }
     public String getRecipeId() {
