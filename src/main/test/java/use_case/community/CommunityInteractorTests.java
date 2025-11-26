de7ed1df596d45ddae54c87a173693ff2908bc10
Jingtian Hu <jt.hu@mail.utoronto.ca>
2025-11-26 16:53:29 -0500

    added testing suite to dependency

---
+++ src/main/test/java/use_case/community/CommunityInteractorTests.java
@@ -0,0 +1,11 @@
+package use_case.community;
+
+
+import java.io.IOException;
+import java.util.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class CommunityInteractorTests {
+}
