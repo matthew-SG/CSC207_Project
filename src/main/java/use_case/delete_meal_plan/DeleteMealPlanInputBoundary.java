eae6b17380f75c58d73b4d682dedfa2f4fe9d0ab
Matthew <matthew.giacomini@mail.utoronto.ca>
2025-11-29 14:34:47 -0500

    Implemented input boundary for the delete meal plan use case

---
+++ src/main/java/use_case/delete_meal_plan/DeleteMealPlanInputBoundary.java
@@ -0,0 +1,15 @@
+package use_case.delete_meal_plan;
+
+import java.io.IOException;
+
+/**
+ * Input boundary for the delete meal plan use case
+ */
+public interface DeleteMealPlanInputBoundary {
+
+    /**
+     * Executes the delete meal plan use case
+     * @param deleteMealPlanInputData the input data for the use case
+     */
+    void execute(DeleteMealPlanInputData deleteMealPlanInputData);
+}
