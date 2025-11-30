6952cfbbd7bfe2e15f6004f76609c3cf4671ded3
Klyvux <ab.bajandoh@gmail.com>
2025-11-29 23:40:22 -0500

    changed these two

--- src/main/java/interface_adapter/search_by_ingr/SearchByIngredientController.java
+++ src/main/java/interface_adapter/search_by_ingr/SearchByIngredientController.java
@@ -1,14 +1,19 @@
 package interface_adapter.search_by_ingr;
+
 import entities.Ingredient;
 import use_case.search_by_ingr.*;
-import java.util.*;
+
+import java.util.List;
+
 public class SearchByIngredientController {
     private final SearchByIngredientInputBoundary interactor;
+
     public SearchByIngredientController(SearchByIngredientInputBoundary interactor) {
         this.interactor = interactor;
     }
-    public SearchByIngredientOutputData  search(List<Ingredient> ingredients, int amountMissing) {
-        SearchByIngredientInputData inputData=new SearchByIngredientInputData(ingredients, amountMissing);
-        return interactor.execute(inputData);
+
+    public void search(List<Ingredient> ingredients, int amountMissing) {
+        SearchByIngredientInputData inputData = new SearchByIngredientInputData(ingredients, amountMissing);
+        interactor.execute(inputData);
     }
 }
