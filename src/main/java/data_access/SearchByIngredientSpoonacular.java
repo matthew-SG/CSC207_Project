0e9ebd363e158081121efb061398ba29130369d5
Klyvux <ab.bajandoh@gmail.com>
2025-11-29 23:45:49 -0500

    changed these to include nutritional information, check for meal type, populate all recipes on call (for approve recipes) use less api calls overall

--- src/main/java/data_access/SearchByIngredientSpoonacular.java
+++ src/main/java/data_access/SearchByIngredientSpoonacular.java
@@ -1,15 +1,16 @@
 package data_access;
 
-import entities.Recipe;
+import use_case.search_by_ingr.SearchByIngredientGateway;
 import entities.Ingredient;
 import okhttp3.*;
 import org.json.*;
+
 import java.io.IOException;
 import java.util.*;
 
-public class SearchByIngredientSpoonacular {
+public class SearchByIngredientSpoonacular implements SearchByIngredientGateway {
     private static final String BYINGREDIENTSURL = "https://api.spoonacular.com/recipes/findByIngredients";
-    private static final String RECIPEINFOURL = "https://api.spoonacular.com/recipes/";
+    private static final String RECIPEINFOURL = "https://api.spoonacular.com/recipes/informationBulk";
     private String apiKey;
 
     public SearchByIngredientSpoonacular(String apiKey) {
@@ -19,8 +20,34 @@ public class SearchByIngredientSpoonacular {
         this.apiKey = "6e0b1d9ab8b94b9dbf723c0203286189";
     }
 
+    @Override
+    public JSONObject searchByIngredients(List<Ingredient> ingredients) {
+            JSONArray recipesJSON = findResipes(ingredients);
 
-    public JSONArray searchByIngredientSpoonacular(List<Ingredient> ingredients) {
+            if (recipesJSON == null || recipesJSON.isEmpty()) {
+                return null;
+            }
+
+            List<Integer> ids = new ArrayList<>();
+
+            for (int i = 0; i < recipesJSON.length(); i++) {
+                JSONObject r = recipesJSON.getJSONObject(i);
+                ids.add(r.getInt("id"));
+            }
+
+        JSONArray infoJSON;
+        if (ids.isEmpty()) {
+            infoJSON = new JSONArray();
+        } else {
+            infoJSON = getInfo(ids);
+        }
+        JSONObject result = new JSONObject();
+        result.put("findResults", recipesJSON);
+        result.put("bulkResults", infoJSON);
+        return result;
+    }
+
+    private JSONArray findResipes(List<Ingredient> ingredients) {
         if (ingredients.isEmpty()) {
             return null;
         }
@@ -55,40 +82,33 @@ public class SearchByIngredientSpoonacular {
             throw new RuntimeException(e);
         }
     }
-    //The api only allows 2 calls per second, so we cant populate the recipe with its steps and ingredient in the same method
-    public void populateRecipeDetails(Recipe recipe) {
-        if (recipe.getIngredients() != null && !recipe.getIngredients().isEmpty()
-                && recipe.getSteps() != null && !recipe.getSteps().isEmpty()) {
-            return;
-        }
-        String recipeInfoUrl = RECIPEINFOURL+recipe.getRecipeId()+"/information";
-        HttpUrl url = HttpUrl.parse(recipeInfoUrl).newBuilder().addQueryParameter("apiKey", apiKey).build();
-        final OkHttpClient client = new OkHttpClient.Builder().build();
-        Request request = new Request.Builder().url(url).get().build();
-        try{
-            Response response = client.newCall(request).execute();
-            final JSONObject responseBody = new JSONObject(response.body().string());
-            if (responseBody.isEmpty()) throw new IOException("Empty response");
-            //setting the ingredients
-            JSONArray extendedIngredients = responseBody.optJSONArray("extendedIngredients");
-            List<Ingredient> ingredients = new ArrayList<>();
-            for(int i=0;extendedIngredients != null&&i<extendedIngredients.length();i++){
-                JSONObject ingredient = extendedIngredients.getJSONObject(i);
-                String ingredientName = ingredient.getString("name");
-                double ingredientQuantity = ingredient.getDouble("amount");
-                String unit = ingredient.getString("unit");
-                ingredients.add(new Ingredient(ingredientName, ingredientQuantity, unit));
-            }
-            recipe.setIngredients(ingredients);
-            //setting the steps
-            String instructions = responseBody.optString("instructions", null);
-            String stepText="";
-            if (instructions!=null&& !instructions.isEmpty())
-                stepText = instructions.trim();
-            else
-                stepText = "No instructions provided";
-            recipe.setSteps(stepText);
 
+    public JSONArray getInfo(List<Integer> ids) {
+        if (ids.isEmpty()) {
+            return null;
+        }
+        List<String> recIds=new ArrayList<>();
+        for (Integer id : ids) {
+            recIds.add(id.toString());
+        }
+        String recipeIds = String.join(",", recIds);
+        HttpUrl url = HttpUrl.parse(RECIPEINFOURL).newBuilder().
+                addQueryParameter("apiKey", apiKey).
+                addQueryParameter("ids", recipeIds).
+                addQueryParameter("includeNutrition", "true").
+                build();
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .build();
+        final OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            String rspns = response.body().string();
+            final JSONArray responseBody = new JSONArray(rspns);
+            if (responseBody.length() == 0) throw new IOException("Empty response");
+            return responseBody;
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
