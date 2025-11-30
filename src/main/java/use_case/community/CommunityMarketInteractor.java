5ef692edddb4c5fbab056924bb746bc1de822f0a
Jingtian Hu <jt.hu@mail.utoronto.ca>
2025-11-29 20:31:54 -0500

    updated interactor to have a like recipe flow

--- src/main/java/use_case/community/CommunityMarketInteractor.java
+++ src/main/java/use_case/community/CommunityMarketInteractor.java
@@ -1,10 +1,12 @@
 package use_case.community;
 
+import entities.Rating;
 import entities.User;
 import use_case.community.input_data.CommunityLikeRecipeInputData;
 import use_case.community.input_data.CommunityPoseSelectionInputData;
 import use_case.community.input_data.CommunityPublishInputData;
 import use_case.community.input_data.CommunityRecipeSelectionInputData;
+import use_case.community.output_data.CommunityLikeRecipeOutputData;
 import use_case.community.output_data.CommunityLikedRecipesOutputData;
 import use_case.community.output_data.CommunityPublishSuccessOutputData;
 import use_case.community.output_data.CommunityRatingsOutputData;
@@ -12,12 +14,14 @@ import use_case.community.output_data.CommunitySelectedRecipeOutputData;
 
 public class CommunityMarketInteractor implements CommunityInputBoundary {
     private CommunityDataAccessInterface communityDAO;
+    private CommunityUserRecipeDataAccessInterface communityLikeRecipeDAO;
     private CommunityOutputBoundary communityPresenter;
 
     public CommunityMarketInteractor(CommunityDataAccessInterface communityDAO,
-                                     CommunityOutputBoundary communityPresenter) {
+                                     CommunityOutputBoundary communityPresenter, CommunityUserRecipeDataAccessInterface communityLikeRecipeDAO) {
         this.setCommunityDAO(communityDAO);
         this.setCommunityPresenter(communityPresenter);
+        this.communityLikeRecipeDAO = communityLikeRecipeDAO;
     }
 
     @Override
@@ -30,13 +34,36 @@ public class CommunityMarketInteractor implements CommunityInputBoundary {
     }
 
     /**
-     * Get
+     * Grab the recipe and add it to user's liked recipe and save.
      *
      * @param data
      */
     @Override
     public void likeRecipe(CommunityLikeRecipeInputData data) {
+        boolean found = false;
+        Rating queriedRating = null;
+        for (Rating rating: this.communityDAO.getCurrentRatings()) {
+            if (rating.getRatingId() == data.getRatingId()){
+                found = true;
+                queriedRating = rating;
+                break;
+            }
+        }
+        if (!found){
+            this.communityPresenter.prepareFailView("Rating not found");
+            return;
+        }
+        if (queriedRating == null || queriedRating.getDetailedRecipe() == null) {
+            this.communityPresenter.prepareFailView("Detailed recipe unavailable for this rating yet.");
+            return;
+        }
 
+        this.communityLikeRecipeDAO.saveRecipeToUser(data.getUsername(), queriedRating.getDetailedRecipe());
+        this.communityPresenter.prepareAddSucc(
+                new CommunityLikeRecipeOutputData(
+                        getCommunityDAO().getCurrentRatings()
+                )
+        );
     }
 
     @Override
@@ -46,10 +73,9 @@ public class CommunityMarketInteractor implements CommunityInputBoundary {
             return;
         }
 
-        User user = new User(data.getUserName(), "dummy", null, null, null);
         communityPresenter.prepareRecipeSelection(
                 new CommunityLikedRecipesOutputData(
-                        getCommunityDAO().getLikedRecipes(user)
+                        getCommunityDAO().getLikedRecipes(data.getUserName())
                 )
         );
     }
