80d787a1bfe0f6be35ae8cfd5a01597ccfee0e5c
Adish <adish.singh@mail.utoronto.ca>
2025-11-29 19:26:38 -0500

    Added the fail view to the output boundary

--- src/main/java/use_case/likedRecipeList/LikedRecipeOutputBoundary.java
+++ src/main/java/use_case/likedRecipeList/LikedRecipeOutputBoundary.java
@@ -5,4 +5,5 @@ import use_case.step_by_step.StepByStepInputData;
 public interface LikedRecipeOutputBoundary {
     void prepareLikedRecipeView(LikedRecipeOutputData likedRecipeOutputData);
     void prepareHandsfree(StepByStepInputData stepByStepInputData);
+    void prepareFailView(String error);
 }
