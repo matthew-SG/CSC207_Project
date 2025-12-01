dfdc8782ca0906b812779407597b7b5e855bae78
Matthew <matthew.giacomini@mail.utoronto.ca>
2025-12-01 02:31:46 -0500

    Fixed most, if not all, checkstyle issues in the entity layer, and also fixed some in the interactor for the SearchByIngredientInteractor.

--- src/main/java/entities/GroceryList.java
+++ src/main/java/entities/GroceryList.java
@@ -2,6 +2,9 @@ package entities;
 
 import java.util.List;
 
+/**
+ * Class representing the Grocery List entity.
+ */
 public final class GroceryList {
     private final List<Ingredient> items;
 
--- src/main/java/entities/UserFactory.java
+++ src/main/java/entities/UserFactory.java
@@ -7,6 +7,15 @@ import java.util.List;
  */
 public class UserFactory {
 
+    /**
+     * Creates a user object.
+     * @param name name of the user
+     * @param password password of the user
+     * @param savedRecipes saved recipes of the user
+     * @param mealPlans meal plans of the user
+     * @param groceryList grocery list of the user
+     * @return the new user
+     */
     public User create(String name, String password, List<Recipe> savedRecipes, List<MealPlan> mealPlans,
                        GroceryList groceryList) {
         return new User(name, password, savedRecipes, mealPlans, groceryList);
