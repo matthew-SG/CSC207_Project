212187c6cd76052039749bd24125ca4dc19e41a6
Adish <adish.singh@mail.utoronto.ca>
2025-11-30 19:48:20 -0500

    Updated the liked recipe input data to store the image and nutritional info as well

--- src/main/java/use_case/likedRecipeList/LikedRecipeInputData.java
+++ src/main/java/use_case/likedRecipeList/LikedRecipeInputData.java
@@ -1,5 +1,7 @@
 package use_case.likedRecipeList;
 
+import java.util.Map;
+
 /**
  * The Input Data for the Liked Recipe List Use Case
  */
@@ -7,17 +9,31 @@ package use_case.likedRecipeList;
 public class LikedRecipeInputData {
     private final int id;
     private final String recipeName;
+    private final String image;
+    private Map<String, Double> nutrition;
 
-    public LikedRecipeInputData(int id, String recipeName) {
+    public LikedRecipeInputData(int id, String recipeName, String image, Map<String, Double> nutrition) {
         this.id = id;
         this.recipeName = recipeName;
+        this.image = image;
+        this.nutrition = nutrition;
     }
 
     int getId() {
+
         return id;
     }
 
     String getRecipeName() {
+
         return recipeName;
+    }
+
+    String getImage() {
+        return image;
+    }
+
+    Map<String, Double> getNutrition() {
+        return nutrition;
     }
 }
