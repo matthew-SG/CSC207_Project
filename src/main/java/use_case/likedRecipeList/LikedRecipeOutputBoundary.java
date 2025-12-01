8eafb96c9b3b0eee9e5b51d28e6cdd40b650674d
Adish <adish.singh@mail.utoronto.ca>
2025-11-30 19:55:59 -0500

    Fixed a style issue in the output boundary, and updated the output data to show the changes in nutrition and image

--- src/main/java/use_case/likedRecipeList/LikedRecipeOutputBoundary.java
+++ src/main/java/use_case/likedRecipeList/LikedRecipeOutputBoundary.java
@@ -3,6 +3,7 @@ package use_case.likedRecipeList;
 import use_case.step_by_step.StepByStepInputData;
 
 public interface LikedRecipeOutputBoundary {
+
     void prepareLikedRecipeView(LikedRecipeOutputData likedRecipeOutputData);
     void prepareHandsfree(StepByStepInputData stepByStepInputData);
     void prepareFailView(String error);
