b1f0414cde047a0178eabaf2966d052a5dca3531
Matthew <matthew.giacomini@mail.utoronto.ca>
2025-12-02 12:33:48 -0500

    Updated interactor to cover perfect case with a different strategy

--- src/test/java/use_case/meal_plan/MealPlanInteractorTests.java
+++ src/test/java/use_case/meal_plan/MealPlanInteractorTests.java
@@ -422,4 +422,104 @@ public class MealPlanInteractorTests {
         interactor.execute(mealPlanInputData);
     }
 
+    /**
+     * Tests the interactor when the user has a different strategy with a perfect match
+     */
+    @Test
+    public void successDifferentStrategyPerfectMatch() {
+        {
+            // Values of non-negative input data do not matter as the returned meal plan should just be the three recipes
+            //      in the user's liked recipes, in the same order
+            MealPlanInputData mealPlanInputData = new MealPlanInputData("50", "50", "50", "50");
+            // Creates temp DAO for testing, api key doesn't matter
+            InMemoryUserDataAccessObject userRepository = new InMemoryUserDataAccessObject("a");
+
+            // Creating a user to test the interactor on
+            userRepository.signupUser("Matthew", "password");
+            userRepository.login("Matthew", "password");
+
+            // Ingredients have no impact on the success
+            List<Ingredient> ingredients = new ArrayList<>();
+            ingredients.add(new Ingredient("salt", 100, "kg"));
+
+            // Four different nutritional infos for four different recipes
+            Map<String, Double> nutritionalInfo1 = new HashMap<>();
+            nutritionalInfo1.put("Calories", 5.0);
+            nutritionalInfo1.put("Carbohydrates", 5.0);
+            nutritionalInfo1.put("Fat", 5.0);
+            nutritionalInfo1.put("Protein", 5.0);
+
+            Map<String, Double> nutritionalInfo2 = new HashMap<>();
+            nutritionalInfo2.put("Calories", 15.0);
+            nutritionalInfo2.put("Carbohydrates", 15.0);
+            nutritionalInfo2.put("Fat", 15.0);
+            nutritionalInfo2.put("Protein", 15.0);
+
+            Map<String, Double> nutritionalInfo3 = new HashMap<>();
+            nutritionalInfo3.put("Calories", 15.0);
+            nutritionalInfo3.put("Carbohydrates", 15.0);
+            nutritionalInfo3.put("Fat", 15.0);
+            nutritionalInfo3.put("Protein", 15.0);
+
+            Map<String, Double> nutritionalInfo4 = new HashMap<>();
+            nutritionalInfo4.put("Calories", 20.0);
+            nutritionalInfo4.put("Carbohydrates", 20.0);
+            nutritionalInfo4.put("Fat", 20.0);
+            nutritionalInfo4.put("Protein", 20.0);
+
+            // 4 different recipes with 4 different nutritional compositions
+            Recipe recipe1 = new Recipe(1234, "spaghetti", "idontexist.jpg", ingredients,
+                    "italian", nutritionalInfo1);
+            Recipe recipe2 = new Recipe(1324, "linguini", "idontexist.jpg", ingredients,
+                    "italian", nutritionalInfo2);
+            Recipe recipe3 = new Recipe(3124, "fettuccine", "idontexist.jpg", ingredients,
+                    "italian", nutritionalInfo3);
+            Recipe recipe4 = new Recipe(5678, "tortellini", "iDOexist.jpg", ingredients,
+                    "italian", nutritionalInfo4);
+
+            List<Recipe> likedRecipes = userRepository.getSavedRecipes();
+            likedRecipes.add(recipe1);
+            likedRecipes.add(recipe2);
+            likedRecipes.add(recipe3);
+            likedRecipes.add(recipe4);
+
+            // The output recipes should be the last 3 as per the algorithm, in the order they appear in the user's
+            //      liked recipes
+            String[] expectedNames = {"linguini", "fettuccine", "tortellini"};
+            String[] expectedImages = {"idontexist.jpg", "idontexist.jpg", "iDOexist.jpg"};
+
+            List<List<String[]>> expectedIngredients = new ArrayList<>();
+            List<String[]> nestedIngredients = new ArrayList<>();
+            expectedIngredients.add(nestedIngredients);
+            expectedIngredients.add(nestedIngredients);
+            expectedIngredients.add(nestedIngredients);
+            nestedIngredients.add(new String[]{"salt", "100.0", "kg"});
+
+            List<Map<String, Double>> expectedNutritionalInfo = new ArrayList<>();
+            expectedNutritionalInfo.add(nutritionalInfo2);
+            expectedNutritionalInfo.add(nutritionalInfo3);
+            expectedNutritionalInfo.add(nutritionalInfo4);
+
+            // Presenter to see if use case works as expected (returns correct names and images,
+            //      sizes of structures are correct)
+            MealPlanOutputBoundary presenter = new MealPlanOutputBoundary() {
+                @Override
+                public void prepareSuccessView(MealPlanOutputData mealPlanOutputData) {
+                    assertEquals(mealPlanOutputData.getIngredients().size(), expectedIngredients.size());
+                    assertArrayEquals(mealPlanOutputData.getRecipeImages(), expectedImages);
+                    assertArrayEquals(mealPlanOutputData.getRecipeNames(), expectedNames);
+                    assertEquals(mealPlanOutputData.getNutritionalValues().size(), expectedNutritionalInfo.size());
+                }
+
+                @Override
+                public void prepareFailView(String listError, String inputError) {
+                    fail("Use case failure is unexpected");
+                }
+            };
+
+            MealPlanInputBoundary interactor = new MealPlanInteractor(userRepository, presenter);
+            interactor.setStrategy(new PrioritizeCaloriesErrorStrategy());
+            interactor.execute(mealPlanInputData);
+        }
+    }
 }
